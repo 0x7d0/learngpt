1bef8d8ce85a12bbc8d7fd86810208f5a7001ff0
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-04-25 14:54:26 +0200

    userprofile minimal

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,37 +1,9 @@
-import Avataaars from 'react-avataaars';
-
 export default function UserProfile({ userProfile }) {
-  console.log('userProfile:', userProfile);
-
-  return (
-    <div className="flex items-center space-x-3 mt-8">
-      <div className="flex-shrink-0">
-        <div className="relative">
-          <div className="absolute inset-0 rounded-full overflow-hidden">
-            <Avataaars
-              style={{ width: '72px', height: '72px' }}
-              avatarStyle="Circle"
-              topType="ShortHairShortWaved"
-              accessoriesType="Prescription01"
-              hairColor="BrownDark"
-              facialHairType="BeardMedium"
-              facialHairColor="BrownDark"
-              clotheType="Hoodie"
-              clotheColor="Heather"
-              eyeType="Squint"
-              eyebrowType="UpDownNatural"
-              mouthType="Twinkle"
-              skinColor="Light"
-            />
-          </div>
-        </div>
+    return (
+      <div className="mt-8">
+        <h2 className="text-lg font-medium text-gray-900">{userProfile.name}</h2>
+        <p className="text-gray-500">{userProfile.bio}</p>
       </div>
-      <div className="min-w-0 flex-1">
-        <h2 className="text-lg font-medium text-gray-900">
-          {userProfile?.firstName} {userProfile?.lastName}
-        </h2>
-        <p>{userProfile?.bio}</p>
-      </div>
-    </div>
-  );
-}
+    );
+  }
+  
