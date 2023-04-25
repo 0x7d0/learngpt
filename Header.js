bc608f20b9e6da23826ac0c4f6167227c93b2662
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-04-25 11:39:14 +0200

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 py-6 px-8">
+      <h1 className="text-white text-3xl font-bold">Your Project Name</h1>
+    </header>
+  );
+};
+
+export default Header;
