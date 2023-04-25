12300b40fd460c4739f2835225ae9bf082e3799b
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-04-25 14:56:48 +0200

    userprofile fix

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,9 +1,32 @@
-export default function UserProfile({ userProfile }) {
-    return (
-      <div className="mt-8">
-        <h2 className="text-lg font-medium text-gray-900">{userProfile.name}</h2>
-        <p className="text-gray-500">{userProfile.bio}</p>
+import React from 'react';
+
+const UserProfile = () => {
+  return (
+    <aside className="bg-white w-1/4 p-6 rounded-lg shadow-md">
+      {/* User Profile */}
+      <div className="mb-6">
+        <h2 className="text-xl font-bold mb-2">Your Name</h2>
+        <p>Experience: 500 XP</p>
+        <p>Skills: JavaScript, React, Next.js</p>
       </div>
-    );
-  }
-  
+
+      {/* Token Wallet */}
+      <div className="mb-6">
+        <h2 className="text-xl font-bold mb-2">Token Wallet</h2>
+        <p>Balance: 10,000 tokens</p>
+      </div>
+
+      {/* Social Media & DMs */}
+      <div>
+        <h2 className="text-xl font-bold mb-2">Connect</h2>
+        <ul>
+          <li>Twitter</li>
+          <li>LinkedIn</li>
+          <li>Direct Messages</li>
+        </ul>
+      </div>
+    </aside>
+  );
+};
+
+export default UserProfile;
